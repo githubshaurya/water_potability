5bfc36b10b3d0a7cc54ddc5556a00f3bfdcc1677
githubshaurya <shauryaomar94@gmail.com>
2025-06-14 11:39:56 +0000

    Add pipeline with median

--- src/models/model_building.py
+++ src/models/model_building.py
@@ -43,7 +43,7 @@ def save_model(model: RandomForestClassifier, model_name: str) -> None:
 def main():
     try:
         params_path = "params.yaml"
-        data_path = "./data/processed/train_processed_mean.csv"
+        data_path = "./data/processed/train_processed_median.csv"
         model_name = "model.pkl"
 
         n_estimators = load_params(params_path)
